ea5e7d10cade80bd63dd101e54555d21f688fca2
peeyush24 <peeyush.pandey@truminds.co.in>
2021-04-01 20:15:12 +0530

    Allowed localhost

--- src/app.js
+++ src/app.js
@@ -1,15 +1,32 @@
-const express= require('express')
+const express = require('express')
 require('./db/mongoose')
 const path = require('path')
-const http= require("http")
-const {cpu_model,memory_model} =require('./models/sys_data')
+const http = require("http")
+const {cpu_model, memory_model} = require('./models/sys_data')
+var cors = require('cors')
 
-const app= express()
+const allowedOrigins = ["http://localhost:3000","http://localhost:4000"];
+
+const app = express()
+
+app.use(
+    cors({
+        origin: function(origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                var msg =
+                    "The CORS policy for this site does not " +
+                    "allow access from the specified Origin.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        }
+    })
+); 
 
 const port= process.env.PORT || 4000
 
 app.use(express.json())
-
 
 app.get('/data', async(req,res) => {
     console.log(req.body)
@@ -32,7 +49,7 @@ app.get('/data', async(req,res) => {
 
 const server = http.createServer(app)
 
-server.listen(4000,()=>{
+app.listen(4000,()=>{
     console.log("server up on 4000")
 })
 
